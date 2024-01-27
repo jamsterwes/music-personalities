c059a2e9b1fa833a004006ea6d5298617f1c4735
Wesley Taylor <jamsterwes@gmail.com>
2024-01-27 02:39:55 -0600

    still broken haha

--- src/components/SongList/Song.tsx
+++ src/components/SongList/Song.tsx
@@ -1,4 +1,14 @@
-export async function Song({ songID, token }: { songID: string, token: string }) {
+import { RiAlbumFill } from "react-icons/ri";
+import { BsFillPersonFill } from "react-icons/bs";
+import { BsFillPeopleFill } from "react-icons/bs";
+import { BsExplicitFill } from "react-icons/bs";
+import { IoClose } from "react-icons/io5";
+
+export async function Song({ songID, token, onClose }: {
+  songID: string,
+  token: string,
+  onClose: () => void
+}) {
   // Get the link to the track's album art
   const res = await fetch(`https://api.spotify.com/v1/tracks/${songID}`, {
     headers: {
@@ -6,18 +16,43 @@ export async function Song({ songID, token }: { songID: string, token: string })
     }
   });
   
-  const data = await res.json();
+  const data = await res.json() as {
+    album: {
+      images: { url: string }[],
+      name: string
+    },
+    name: string,
+    artists: {
+      name: string
+    }[],
+    explicit: boolean
+  };
 
   const albumArtLink = data.album.images[0].url;
+  const songName = data.name;
+  const songAlbumName = data.album.name;
+  const songArtists = data.artists.map(artist => artist.name).join(", ");
+  const isMultipleArtists = data.artists.length > 1;
+  const isExplicit = data.explicit;
 
   
   return (
-    <div className="grid grid-cols-3 items-center h-48 border-b-2">
-      <img src={albumArtLink} alt="Album Art" className="p-4 h-48 w-auto" />
-      <div className="col-span-2 grid grid-rows-3">
-        <span>Night Shift</span>
-        <span>Lucy Dacus</span>
-        <span>Historian</span>
+    <div className="flex items-center h-36 border-b-2 px-4">
+      <IoClose className="text-4xl cursor-pointer hover:text-red-100 active:text-red-200" onClick={onClose} />
+      <img src={albumArtLink} alt="Album Art" className="p-4 h-36 w-auto select-none" draggable="false" />
+      <div className="col-span-2 grid grid-rows-4">
+        <span className="text-4xl flex gap-3 font-bold row-span-2 items-center">
+          {isExplicit ? <BsExplicitFill className="text-2xl text-stone-300" /> : <></>}
+          {songName}
+        </span>
+        <span className="text-xl flex gap-3 items-center font-medium">
+          {isMultipleArtists ? <BsFillPeopleFill /> : <BsFillPersonFill />}
+          {songArtists}
+        </span>
+        <span className="text-lg flex gap-3 items-center">
+          <RiAlbumFill />
+          {songAlbumName}
+        </span>
       </div>
     </div>
   );
--- src/components/SongList/SongList.tsx
+++ src/components/SongList/SongList.tsx
@@ -1,11 +1,27 @@
+'use client';
+
 import { FaPlus } from 'react-icons/fa6';
 import { Song } from './Song';
+import { useState } from 'react';
 
 export function SongList({ token }: { token: string }) {
+
+  const [songs, setSongs] = useState<string[]>([
+    "1yYlpGuBiRRf33e1gY61bN",
+    "4zlbKky2yA657Sk5rekZoR",
+    "0snQkGI5qnAmohLE7jTsTn"
+  ]);
+
+  const onClose = (i: number) => {
+    setSongs([...songs.slice(0, i), ...songs.slice(i)]);
+  }
+
   return (
-    <div className="text-white text-4xl border-white border-2 w-4/5 flex flex-col">
-      <Song songID="1yYlpGuBiRRf33e1gY61bN" token={token} />      
-      <div className="py-2 flex justify-center items-center gap-6 cursor-pointer hover:bg-stone-800">
+    <div className="text-white border-white border-2 w-4/5 flex flex-col">
+      {songs.map((songID, i) => 
+        <Song key={songID} songID={songID} token={token} onClose={() => onClose(i)} />
+      )}
+      <div className="text-4xl py-2 flex justify-center items-center gap-6 cursor-pointer hover:bg-stone-800">
         <FaPlus /> Add Song
       </div>
     </div>
